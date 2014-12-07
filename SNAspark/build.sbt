6c6ed738efbb1a39c1b4096962d42f119770601b
Thomas Carette <tcarette@gmail.com>
2014-12-07 15:24:52 +0100

    Add simple spark sbt project: this version simply extract a json schema (edit abolute path in code)

--- SNAspark/build.sbt
+++ SNAspark/build.sbt
@@ -10,4 +10,6 @@ libraryDependencies += "org.apache.spark" %% "spark-core" % "1.1.0"
 
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "1.1.0"
 
+libraryDependencies += "org.apache.spark" %% "spark-graphx" % "1.1.0"
+
 resolvers += "Akka Repository" at "http://repo.akka.io/releases/"
--- SNAspark/src/main/scala/test.scala
+++ SNAspark/src/main/scala/test.scala
@@ -1,31 +1,35 @@
 
-import org.apache.spark.sql
-import org.apache.spark.sql._
+//import org.apache.spark.sql
+//import org.apache.spark.sql._
 import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
+//import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
+//import org.apache.spark._
+//import org.apache.spark.graphx._
+//import org.apache.spark.rdd.RDD
+//import ch.epfl.lamp.compiler.msil.Attribute
 
 object test1 {
   def main(args: Array[String]) {
-    val fileName = "/Users/tcarette/Documents/Projets-Divers/data4good_and_co/handsOnGraph/data/profilesFromKeywords.json" // Should be srme file on your system
+    val fileName = "/Users/tcarette/Documents/Projets-Divers/data4good_and_co/handsOnGraph/data/belgian_groups.json" // Should be srme file on your system
 
-//  Spark context
+    //  Spark context
     val conf = new SparkConf().setAppName("Test 1")
+                              .setMaster("local")
     val sc = new SparkContext(conf)
 
-//    simple text analysis
+    //    simple text analysis
     val rawData = sc.textFile(fileName, 2).cache()
     val numAs = rawData.filter(line => line.contains("a")).count()
     val numBs = rawData.filter(line => line.contains("b")).count()
     println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
 
-//  SQL context
-//    val sqlc = new org.apache.spark.sql.SQLContext(sc)
+    //  SQL context
+    val sqlc = new org.apache.spark.sql.SQLContext(sc)
 //    import sqlc.createSchemaRDD
 
-//  Schema
-//    val profiles = sqlc.jsonFile(fileName)
-//    profiles.printSchema()
-
+    //  Schema
+    val profiles = sqlc.jsonFile(fileName)
+    profiles.printSchema()
   }
 }
